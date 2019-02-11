b43a686e777a7aeefce058b13aba9fcf39a934a6
zach <zachbroad@gmail.com>
2019-02-11 09:16:31 -0500

    colors changed

--- src/components/ServiceItem/index.js
+++ src/components/ServiceItem/index.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import './styles.sass';
+import { FaCode } from 'react-icons/fa';
 
 export const ServiceItem = ({ data }) => (
   <div className="service-container">
     <div className="service-header">
-      <Link to={data.frontmatter.path} className="title is-4">
-        <h2 className="service-title">{data.frontmatter.title}</h2>
-      </Link>
+      <div>
+        <FaCode/>
+        <Link to={data.frontmatter.path} className="title is-4">
+          <h2 className="service-title">{data.frontmatter.title}</h2>
+        </Link>
+      </div>
       <h4 className="sub">{data.frontmatter.description}</h4>
     </div>
     <div className="service-body">
       {/*<Link to={data.frontmatter.path}>*/}
-        {/*<button className="button is-fullwidth">Learn more</button>*/}
+      {/*<button className="button is-fullwidth">Learn more</button>*/}
       {/*</Link>*/}
     </div>
   </div>
